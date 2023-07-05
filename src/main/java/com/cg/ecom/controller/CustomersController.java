f8d27067d7c12317d6bdf079af4590216eb7e9e4
Haneesh Jallipalli <55525003+HaneeshDevops@users.noreply.github.com>
2023-07-05 16:28:58 +0530

    Update CustomersController.java

--- src/main/java/com/cg/ecom/controller/CustomersController.java
+++ src/main/java/com/cg/ecom/controller/CustomersController.java
@@ -32,7 +32,7 @@ public class CustomersController {
 
 	
 
-	@PostMapping("/addCustomerlikhithaGopireddy")
+	@PostMapping("/addCustomer")
 	public ResponseEntity<?> addCustomersnew(@Valid @RequestBody AddCustomersDTO customers, BindingResult result) {
 	    if (result.hasErrors()) {
 	        Map<String, String> errors = new HashMap<>();
